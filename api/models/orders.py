843e1124032c28b200d5d031ba167be1212dc639
DuberMM21 <dubermm21@gmail.com>
2025-04-11 13:45:25 -0400

    fix: Added relationships on orders.py models and edited customer_name for customer_id on models and schemas directories

--- api/models/orders.py
+++ api/models/orders.py
@@ -8,8 +8,10 @@ class Order(Base):
     __tablename__ = "orders"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    customer_name = Column(String(100))
+    customer_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     order_date = Column(DATETIME, nullable=False, server_default=str(datetime.now()))
     description = Column(String(300))
 
     order_details = relationship("OrderDetail", back_populates="order")
+    customer = relationship("User", back_populates="orders")
+    reviews = relationship("Review", back_populates="order")
