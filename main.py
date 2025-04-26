7b2dfe1cad4b55ccdc42da7340b8d0f8819608f8
Meet <pmeet9341@gmail.com>
2025-04-26 18:35:38 -0400

    fix: updated main.py to include users and payments routers in branch

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+# main.py
+
 from fastapi import FastAPI
-from controllers import users, payments
+from api.controllers import users, payments
 
 app = FastAPI()
-
-app.include_router(users.router)
-app.include_router(payments.router)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+# Include your routers
+app.include_router(users.router, prefix="/users", tags=["Users"])
+app.include_router(payments.router, prefix="/payments", tags=["Payments"])
