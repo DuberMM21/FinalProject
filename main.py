a328c9deab5c837b697072e5a05873191e93b57b
Meet <pmeet9341@gmail.com>
2025-04-26 18:31:51 -0400

    fix: updated main.py to include users and payments routers

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI
-from controllers import users, payments
 
 app = FastAPI()
-
-app.include_router(users.router)
-app.include_router(payments.router)
 
 @app.get("/")
 def read_root():
